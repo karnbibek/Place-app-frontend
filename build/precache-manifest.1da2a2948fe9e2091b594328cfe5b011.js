03c62b36a180b136617fb526680c3b6e1b5ade03
Bibek <karnabibek96@gmail.com>
2021-01-07 12:27:38 +0530

    first commit

---
+++ build/precache-manifest.1da2a2948fe9e2091b594328cfe5b011.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a19d5059f67372f1b9cbf8aaa22ad2a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "882e9b155a582ba6d965",
+    "url": "/static/css/0.e4959755.chunk.css"
+  },
+  {
+    "revision": "fdbf548e4cb254af3bf5",
+    "url": "/static/css/4.4e5eea1e.chunk.css"
+  },
+  {
+    "revision": "4f976712ab01d3c0ae24",
+    "url": "/static/css/5.4726fe12.chunk.css"
+  },
+  {
+    "revision": "3e4230aee81c2b63757b",
+    "url": "/static/css/6.fef62966.chunk.css"
+  },
+  {
+    "revision": "28ea8f57431a876004de",
+    "url": "/static/css/7.e29c9170.chunk.css"
+  },
+  {
+    "revision": "31f08af7bd0bdff2498e",
+    "url": "/static/css/8.73479ebd.chunk.css"
+  },
+  {
+    "revision": "1a40a9029a97dc426f9d",
+    "url": "/static/css/main.8a54f635.chunk.css"
+  },
+  {
+    "revision": "882e9b155a582ba6d965",
+    "url": "/static/js/0.fcbec771.chunk.js"
+  },
+  {
+    "revision": "a939844fc58b0cf3971c",
+    "url": "/static/js/3.c96e1502.chunk.js"
+  },
+  {
+    "revision": "fdbf548e4cb254af3bf5",
+    "url": "/static/js/4.f955802b.chunk.js"
+  },
+  {
+    "revision": "4f976712ab01d3c0ae24",
+    "url": "/static/js/5.51b0b822.chunk.js"
+  },
+  {
+    "revision": "3e4230aee81c2b63757b",
+    "url": "/static/js/6.247b6ebe.chunk.js"
+  },
+  {
+    "revision": "28ea8f57431a876004de",
+    "url": "/static/js/7.f95101da.chunk.js"
+  },
+  {
+    "revision": "31f08af7bd0bdff2498e",
+    "url": "/static/js/8.bb89d024.chunk.js"
+  },
+  {
+    "revision": "1a40a9029a97dc426f9d",
+    "url": "/static/js/main.95ffb81c.chunk.js"
+  },
+  {
+    "revision": "e267d560dc5710651bf4",
+    "url": "/static/js/runtime-main.66babc96.js"
+  }
+]);
